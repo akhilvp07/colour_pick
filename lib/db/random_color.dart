ca97ad711f423ed2cf13d42409ab0aec223e72f1
Akhil Valsamrithan <avalsamrithan@rbbn.com>
2022-02-17 19:26:13 +0530

    Answer buttons added

--- lib/db/random_color.dart
+++ lib/db/random_color.dart
@@ -1,17 +1,25 @@
 import 'dart:math';
 
 import 'package:colour_pick/db/colors_db.dart';
+import 'package:flutter/cupertino.dart';
 
 abstract class RandomColorFunctions {
-  ColorObject getRandomColor();
+  ColorObject getRandomObject();
+  String getColorString(Color color);
 }
 
 class RandomColor extends RandomColorFunctions {
   @override
-  ColorObject getRandomColor() {
+  ColorObject getRandomObject() {
     final random = Random();
     var i = random.nextInt(colors.length);
     var _colorObj = ColorObject(colors[i]['color'], colors[i]['text']);
     return _colorObj;
+  }
+
+  @override
+  String getColorString(Color color) {
+    var obj = colors.firstWhere((element) => element['color'] == color);
+    return obj['text'];
   }
 }
--- lib/widgets/display.dart
+++ lib/widgets/display.dart
@@ -15,18 +15,32 @@ class Display extends StatefulWidget {
 class _DisplayState extends State<Display> {
   late ColorObject _currentColorObject;
   Color _currentColor = Colors.red; //Holds the correct colour
-  Color _randomColor = colorDB.getRandomColor().color;
-  String _currentText = "Red"; // Holds the correct colour text
+  String _randomText = colorDB.getRandomObject().text;
+  String _currentText = "Red";
+  String _resultText = "Start!"; // Holds the correct colour text
+  int _points = 0;
 
+  List<String> answers = [
+    colorDB.getRandomObject().text,
+    "Red",
+  ];
   @override
   void initState() {
-    Timer.periodic(const Duration(seconds: 2), (timer) {
-      _currentColorObject = colorDB.getRandomColor();
+    Timer.periodic(const Duration(seconds: 3), (timer) {
+      _currentColorObject = colorDB.getRandomObject();
+
       setState(() {
         _currentColor = _currentColorObject.color;
         _currentText = _currentColorObject.text;
-        _randomColor = colorDB.getRandomColor().color;
+        _randomText = colorDB.getRandomObject().text;
+
+        //create the list of answers and shuffle
+        answers.clear();
+        answers.add(_currentText);
+        answers.add(colorDB.getRandomObject().text);
+        answers.shuffle();
       });
+
       //mytimer.cancel() //to terminate this timer
     });
     super.initState();
@@ -35,13 +49,27 @@ class _DisplayState extends State<Display> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Center(
           child: Text(
-            //'Text',
-            _currentText,
+            //Result text
+            _resultText,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              overflow: TextOverflow.ellipsis,
+            ),
+            textAlign: TextAlign.right,
+          ),
+        ),
+        Center(
+          child: Text(
+            //The main coloured text object,
+            _randomText,
             style: TextStyle(
-              color: _randomColor,
+              color: _currentColor,
               fontSize: 100,
               fontWeight: FontWeight.bold,
               overflow: TextOverflow.ellipsis,
@@ -49,7 +77,54 @@ class _DisplayState extends State<Display> {
             textAlign: TextAlign.right,
           ),
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            //Left Button
+            ElevatedButton(
+              onPressed: () {
+                checkAnswer(answers[0]);
+              },
+              child: Text(
+                answers[0],
+                style: const TextStyle(fontSize: 30.0),
+              ),
+            ),
+            //Right Button
+            ElevatedButton(
+              onPressed: () {
+                checkAnswer(answers[1]);
+              },
+              child: Text(
+                answers[1],
+                style: const TextStyle(fontSize: 30.0),
+              ),
+            ),
+          ],
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              'Points: $_points',
+              style: const TextStyle(fontSize: 20.0),
+            ),
+          ),
+        ),
       ],
     );
+  }
+
+  void checkAnswer(String ans) {
+    if (ans == _currentText) {
+      setState(() {
+        _points++;
+        _resultText = "Correct Answer!";
+      });
+    } else {
+      setState(() {
+        _resultText = "Wrong!";
+      });
+    }
   }
 }
